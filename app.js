75fd8c10619973a70ccaf91f673bcebc29cb2d07
jose roman <el-1ero@outlook.com>
2020-02-06 18:19:10 -0400

    ciclo de vida de vue.js

---
+++ app.js
@@ -0,0 +1,45 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: "Ciclo de vida Vue",
+    },
+    beforeCreate() {
+        console.log('beforeCreate');
+    },
+    created() {
+        //Al crear los metodos, observadores y eventos, pero aun no accede al DOM.
+        //Aun no se puede acceder a 'el'
+        console.log('Created');
+    },
+    beforeMount() {
+        //Se ejecuta antes de insertar el DOM
+        console.log('beforeMount');
+    },
+    mounted() {
+        //Se ejecuta al insertar el DOM
+        console.log('mounted');
+    },
+    beforeUpdate() {
+        //Al detectar un cambio
+        console.log('beforeUpdate');
+    },
+    updated() {
+        //Al realizar los cambios
+        console.log('updated');
+    },
+    beforeDestroy() {
+        //Antes de destruir la instancia
+        console.log('beforeDestroy');
+    },
+    destroyed() {
+        //Se destruye toda la instancia :(
+        console.log('destroyed')
+    },
+
+    methods: {
+        destruir() {
+            this.$destroy();
+        }
+    }
+
+});
